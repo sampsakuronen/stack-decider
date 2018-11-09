2006e8377836bc19c68c069bedcbadb4f651ef99
Sampsa Kuronen <sampsa.kuronen@reaktor.com>
2018-11-09 14:14:31 +0200

    Styling and add input

--- main.js
+++ main.js
@@ -67,21 +67,19 @@ const techs = [
   "Hadoop"
 ]
 
-const techsByFirstLetter = groupBy(techs, 0)
-
-function groupBy(xs, key) {
-  return xs.reduce((rv, x) => {
-    (rv[x[key]] = rv[x[key]] || []).push(x);
+function techsByFirstLetter() {
+  return techs.reduce((rv, x) => {
+    (rv[x[0].toUpperCase()] = rv[x[0].toUpperCase()] || []).push(x);
     return rv;
   }, {});
 }
 
 function decideStack(acronym) {
-  const acronymArray = acronym.split("")
+  const acronymArray = acronym.split('')
   const stack = acronymArray.reduce((accu, character) => {
-    const suitableTechs = techsByFirstLetter[character]
+    const suitableTechs = techsByFirstLetter()[character.toUpperCase()]
     if (suitableTechs === undefined) {
-      return accu.concat("NO IDEA")
+      return accu.concat('NO IDEA')
     }
     const randomTech = suitableTechs.sort((a, b) => 0.5 - Math.random())[0]
     return accu.concat(randomTech)
@@ -90,12 +88,23 @@ function decideStack(acronym) {
   return stack
 }
 
-console.log(decideStack("REAKTOR"))
-console.log(decideStack("LOL"))
-console.log(decideStack("GRAND"))
-console.log(decideStack("LAMP"))
-console.log(decideStack("MEGA"))
-console.log(decideStack("MEAN"))
-console.log(decideStack("MACHINE"))
-console.log(decideStack("FOO"))
-console.log(decideStack("BAR"))
+function showStack() {
+  const input = document.querySelector('input[name=acronym]')
+  const stackElement = document.getElementById('stack')
+  stackElement.innerHTML = decideStack(input.value).map((x) => `${x}<br>`).join('')
+}
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function init() {
+  const input = document.querySelector('input[name=acronym]')
+  input.addEventListener('keyup', showStack)
+}
+
+ready(init)
